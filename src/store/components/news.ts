d58dd04470258a8740d48659491973a34c0b9656
tom <tomvanhees2014@gmail.com>
2020-10-21 09:16:35 +0200

    added a Update method to the news store

--- src/store/components/news.ts
+++ src/store/components/news.ts
@@ -1,31 +1,45 @@
-import { NewsItem } from "@/classes/NewsItem";
+import {NewsItem} from "@/classes/NewsItem";
+import index from "@/store";
 
 export const news = {
-  namespaced: true,
-  state: {
-    items: [] as NewsItem[],
-    showNewsItem: false,
-    selectedNewsItem: null
-  },
-  mutations: {
-    set(state: any, data: NewsItem[]) {
-      state.items = data;
+    namespaced: true,
+    state: {
+        items: [] as NewsItem[],
+        showNewsItem: false,
+        selectedNewsItem: null
     },
-    showItem(state: any, data: NewsItem) {
-      state.selectedNewsItem = data;
-      state.showNewsItem = true;
+    mutations: {
+        set(state: any, data: NewsItem[]) {
+            state.items = data;
+        },
+        showItem(state: any, data: NewsItem) {
+            state.selectedNewsItem = data;
+            state.showNewsItem = true;
+        },
+        closeShowItem(state: any) {
+            state.showNewsItem = false;
+            state.selectedNewsItem = null;
+        },
+        add(state: any, data: NewsItem) {
+            state.items.push(data);
+        },
+        update(state: any, data: { index: number, data: NewsItem }) {
+            state.items.splice(index, 1, data);
+        }
     },
-    closeShowItem(state: any) {
-      state.showNewsItem = false;
-      state.selectedNewsItem = null;
+    actions: {
+        setItems(context: any, data: NewsItem[]) {
+            context.commit("set", data);
+        },
+        showItem(context: any, data: NewsItem) {
+            context.commit("showItem", data);
+        },
+        updateItem(context: any, data: NewsItem) {
+            const index = context.state.items.findIndex((item: NewsItem) => item.id === data.id)
+
+            index === -1 ?
+                context.commit('add', data) :
+                context.commit('update', {index: index, data: data})
+        }
     }
-  },
-  actions: {
-    setItems(context: any, data: NewsItem[]) {
-      context.commit("set", data);
-    },
-    showItem(context: any, data: NewsItem) {
-      context.commit("showItem", data);
-    }
-  }
 };
